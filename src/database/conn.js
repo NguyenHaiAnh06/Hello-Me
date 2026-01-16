94ff77e70102d241f0f2bf48a348a5d1a2a6f8cc
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-01-17 00:27:24 +0700

    ket noi sql

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,1 +1,18 @@
-// ket noi co so du lieu
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Haianh2006@',
+    database: 'haianh_software'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Không thể kết nối MySQL: ' + err.message);
+        return;
+    }
+    console.log('Đã kết nối thành công tới Database: haianh_software');
+});
+
+module.exports = connection;
