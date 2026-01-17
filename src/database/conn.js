fb2c0bc58395a381a6af87f5ac8f08fe7aef555b
NguyenHaiAnh06 <tqx4nb4j7r@privaterelay.appleid.com>
2026-01-18 02:49:27 +0700

    update login logic

--- src/database/conn.js
+++ src/database/conn.js
@@ -1,12 +1,10 @@
 const mysql = require('mysql2');
-
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Haianh2006@',
     database: 'haianh_software'
 });
-
 connection.connect((err) => {
     if (err) {
         console.error('Không thể kết nối MySQL: ' + err.message);
@@ -14,5 +12,4 @@ connection.connect((err) => {
     }
     console.log('Đã kết nối thành công tới Database: haianh_software');
 });
-
 module.exports = connection;
